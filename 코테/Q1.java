c078bc19a6b6e0c1a6ddc8cf54ee71bb43e052b6
wxxhyeong <wxxhyeong@gmail.com>
2022-12-16 23:52:19 +0900

    Q1/12월16일
    temp

---
+++ 코테/Q1.java
@@ -0,0 +1,30 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.SequenceInputStream;
+import java.util.StringTokenizer;
+
+public class Q1 {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader((System.in)));
+        System.out.println(br.readLine());
+
+        StringTokenizer stk = new StringTokenizer(br.readLine());
+
+        String A = stk.nextToken();
+        String B = stk.nextToken();
+
+        System.out.println("A = " + A);
+        System.out.println("B = " + B);
+
+
+
+
+    }
+
+    public void solution() {
+
+    }
+}
