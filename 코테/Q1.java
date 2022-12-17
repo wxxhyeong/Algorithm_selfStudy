f076e590b211caa08bef15f94f072c38b799a57b
wxxhyeong <wxxhyeong@gmail.com>
2022-12-17 23:56:08 +0900

    Q1/12월17일
    temp

--- 코테/Q1.java
+++ 코테/Q1.java
@@ -2,13 +2,14 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.SequenceInputStream;
+import java.util.ArrayList;
 import java.util.StringTokenizer;
 
 public class Q1 {
 
     public static void main(String[] args) throws IOException {
 
-        BufferedReader br = new BufferedReader(new InputStreamReader((System.in)));
+        /*BufferedReader br = new BufferedReader(new InputStreamReader((System.in)));
         System.out.println(br.readLine());
 
         StringTokenizer stk = new StringTokenizer(br.readLine());
@@ -18,13 +19,29 @@ public class Q1 {
 
         System.out.println("A = " + A);
         System.out.println("B = " + B);
-
-
+*/
+        Q1 q1 = new Q1();
+        q1.solution();
 
 
     }
 
-    public void solution() {
+    public void solution() throws IOException {
 
+        BufferedReader br = new BufferedReader(new InputStreamReader((System.in)));
+        int test = Integer.parseInt(br.readLine());
+        System.out.println("test = " + test);
+
+        for (int i = 0; i < test; i++) {
+            int n = Integer.parseInt(br.readLine());
+            ArrayList<Integer> numbers = new ArrayList<>(n);
+            StringTokenizer stk = new StringTokenizer(br.readLine());
+
+            for (int j = 0; j < n; j++) {
+                numbers.add(Integer.parseInt(stk.nextToken()));
+            }
+            System.out.println("numbers = " + numbers);
+
+        }
     }
 }
