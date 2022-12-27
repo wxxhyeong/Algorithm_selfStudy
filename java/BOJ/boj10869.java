68829199407178803024dceb60f591465613356e
wxxhyeong <wxxhyeong@gmail.com>
2022-12-28 00:03:42 +0900

    boj10869

---
+++ java/BOJ/boj10869.java
@@ -0,0 +1,19 @@
+/*
+BufferedReader 가 두배 빠름
+ */
+
+import java.util.Scanner;
+
+public class boj10869 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+
+        System.out.println(a + b);
+        System.out.println(a - b);
+        System.out.println(a * b);
+        System.out.println(a / b);
+        System.out.println(a % b);
+    }
+}
