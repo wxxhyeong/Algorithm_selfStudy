0b33cb1d12e6618ed90af2aca89ea55ab8a9e54c
wxxhyeong <wxxhyeong@gmail.com>
2023-01-03 23:51:13 +0900

    백준10926

---
+++ java/BOJ/src/boj10926.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class boj10926 {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        String id = sc.nextLine();
+
+        System.out.println(id+"??!");
+
+    }
+}
