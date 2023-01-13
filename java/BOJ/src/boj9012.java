218b943b44632e37c1225013602c58aa6b19a65b
wxxhyeong <wxxhyeong@gmail.com>
2023-01-13 14:57:44 +0900

    java/boj9012(괄호)/1월13일

---
+++ java/BOJ/src/boj9012.java
@@ -0,0 +1,34 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
+public class boj9012 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(br.readLine());
+
+        for (int i = 0; i < t; i++) {
+            String ps = br.readLine();
+            int cnt = 0;
+
+            for (int j = 0; j < ps.length(); j++) {
+                if (cnt >= 0) {
+                    if (ps.charAt(j)=='(') {
+                        cnt++;
+                    }
+                    else
+                        cnt --;
+                }
+                else
+                    break;
+            }
+
+            if (cnt == 0) {
+                System.out.println("YES");
+            }
+            else
+                System.out.println("NO");
+        }
+    }
+}
