d5f4487104b06f18a840bd69423359c5cbdb6c5e
wxxhyeong <wxxhyeong@gmail.com>
2023-01-31 15:08:11 +0900

    python/boj2504(괄호의값)/1월28일

---
+++ java/BOJ/src/boj1339.java
@@ -0,0 +1,15 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class boj1339 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        List<String> str = new ArrayList<>();
+        int n = sc.nextInt();
+        for (int i = 0; i < n; i++) {
+            str.add(sc.nextLine());
+        }
+
+    }
+}
