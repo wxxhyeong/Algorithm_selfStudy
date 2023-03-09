ba00cc2526ab672382ad6e65e59829f3785bb89e
wxxhyeong <wxxhyeong@gmail.com>
2023-03-09 20:08:01 +0900

    boj4375(1)/3월9일

--- woohyeong/boj4375.java
+++ woohyeong/boj4375.java
@@ -1,7 +1,31 @@
 package woohyeong;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class boj4375 {
-    public static void main(String[] args) {
-        System.out.println("hello world");
+    public static void main(String[] args) throws NumberFormatException, IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        String s;
+        while ((s = br.readLine()) != null) {
+            int c = Integer.parseInt(s);
+            int num = 1;
+            int i = 1; // 자리 수
+
+            while (true) {
+                if (num % c == 0) {
+                    sb.append(i + "\n");
+                    break;
+                }
+
+                num = num * 10 + 1;
+                num %= c;
+                i++;
+            };
+        }
+
+        System.out.println(sb.toString());
     }
 }
