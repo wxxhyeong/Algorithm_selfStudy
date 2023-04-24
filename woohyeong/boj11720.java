57f6b45df41cabcb7f30bcfcde0173f98ecf1959
wxxhyeong <wxxhyeong@gmail.com>
2023-04-24 17:53:56 +0900

    boj11720/4월24일/자바

--- woohyeong/boj11720.java
+++ woohyeong/boj11720.java
@@ -1,17 +1,29 @@
 package woohyeong;
 import java.io.*;
+import java.util.StringTokenizer;
 
 public class boj11720 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
-        char[] numbers = br.readLine().toCharArray();
-        long sum = 0;
 
+        StringTokenizer stk = new StringTokenizer(br.readLine());
+        int[] arr = new int[n];
         for (int i = 0; i < n; i++) {
-            sum += numbers[i] - '0';
+            arr[i] = Integer.parseInt(stk.nextToken());
         }
 
-        System.out.println(sum);
+        int max = 0;
+        int sum = 0;
+        for (int i : arr) {
+            if (max < i) {
+                max = i;
+            }
+            sum += i;
+        }
+
+        System.out.println(sum*100.0/max/n);
+
+
     }
 }
